6fe3dfcf30817af4b7be8e8e9462efbcee2a070f
Daniel Rojo <danielrojo@gmail.com>
2024-11-18 10:16:40 +0100

    revert build.gradle

--- AndroidApplication/build.gradle.kts
+++ AndroidApplication/build.gradle.kts
@@ -2,7 +2,4 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
-    id("com.android.application") version "8.7.0" apply false
-    id("com.android.library") version "8.7.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.20" apply false
 }
